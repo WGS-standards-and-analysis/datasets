d21a8b9f7fa6a2ffd134fbce7c081120ec41cce5
Lee Katz - Aspen <gzu2@cdc.gov>
2016-07-06 09:01:29 -0400

    refactored code; added arguments

--- scripts/excel2tsv.pl
+++ scripts/excel2tsv.pl
@@ -1,85 +1,117 @@
 #!/usr/bin/env perl
+# Author: Chris Gulvik
+# Modified by: Lee Katz <lkatz@cdc.gov>
 
 
 use strict;
-no warnings 'utf8';  #hides 'Wide character in print' error
+use warnings;
+#no warnings 'utf8';  #hides 'Wide character in print' error
+use autodie;
+use File::Basename qw/basename fileparse/;
+use Getopt::Long;
 
+use FindBin qw/$RealBin/;
+use lib "$RealBin/../lib/perl5";
+use Spreadsheet::ParseExcel;
+use Spreadsheet::XLSX;
 
-die usage() if(@ARGV<1);
-my $file = $ARGV[0];
-$file =~ s{\.[^.]+$}{};
+my @supportedExt=qw(.xlsx .xls);
+local $0=basename $0;
+sub logmsg{print STDERR "$0: @_\n"}
 
-if($ARGV[0] =~ /\.xls$/i){
-    use Spreadsheet::ParseExcel;
+exit main();
 
-    my $parser = Spreadsheet::ParseExcel -> new();
-    my $workbook = $parser -> parse($ARGV[0]);
-    if(!defined $workbook){
-        die $parser -> error(), ".\n";
-    }
+sub main{
+  my $settings={};
+  GetOptions($settings,qw(help firstsheet sheetnum=i sep|separator=s)) or die $!;
+  $$settings{sheetnum}=1 if($$settings{firstsheet});
+  $$settings{sep}||="\t";
+  die usage() if(@ARGV<1 || $$settings{help});
 
-    for my $sheet($workbook -> worksheets()){
-        my $sheetName = $sheet -> get_name();
-        my $filename = ">$file"."_$sheetName.tsv";
-        my($row_min,$row_max)=$sheet -> row_range();
-        my($col_min,$col_max)=$sheet -> col_range();
-        open(OUTFILE,$filename);
-        for my $row ($row_min .. $row_max){
-            my @values;
-            for my $col ($col_min .. $col_max){
-                my $cell = $sheet -> get_cell($row, $col);
-                if(defined $cell and $cell -> value() ne ''){
-                    push @values, $cell -> value();
-                }
-                else{
-                    push @values, "\t";
-                }
-            }
-            $" = "\t";
-            print OUTFILE "@values\n";
-        }
-        close OUTFILE;
-    }
-    print "XLS converted into TSV\n";
-}
+  my $file = $ARGV[0];
+  die "ERROR: not found: $file" if(!-e $file);
+  my($name,$dir,$ext)=fileparse($file,@supportedExt);
 
-elsif($ARGV[0] =~ /\.xlsx$/i){
-    use Spreadsheet::XLSX;
+  my $sheetCounter=0;
+  if($ext =~ /\.xls$/i){
 
-    my $workbook = Spreadsheet::XLSX -> new ($ARGV[0]);
-    foreach my $sheet (@{$workbook -> {Worksheet}}){
-        my $sheetName = $sheet->{Name};
-        my $filename = ">$file"."_$sheetName.tsv";
-        open(OUTFILE,$filename);
-        $sheet -> {MaxRow} ||= $sheet -> {MinRow};    
-        foreach my $row ($sheet -> {MinRow} .. $sheet -> {MaxRow}){             
-            $sheet -> {MaxCol} ||= $sheet -> {MinCol};                
-            foreach my $col ($sheet -> {MinCol} .. $sheet -> {MaxCol}){
-                my $cell = $sheet -> {Cells} [$row] [$col];                
-                if($cell){
-                    print OUTFILE $cell -> {Val}, "\t";
-                }
-                else{
-                    print OUTFILE "\t\t";
-                }
-            }
-            print OUTFILE "\n";
-        }
-    }
-    print "XLSX converted into TSV\n";
-}
+      my $parser = Spreadsheet::ParseExcel -> new();
+      my $workbook = $parser -> parse($ARGV[0]);
+      if(!defined $workbook){
+          die $parser -> error(), ".\n";
+      }
 
-else{
-    print "ERROR: unsupported extension\n";
-    print "This script requires a XLS or XLSX extension\n\n";
-    usage();
+      for my $sheet($workbook -> worksheets()){
+          $sheetCounter++;
+          if($$settings{sheetnum} && $sheetCounter!=$$settings{sheetnum}){
+            next;
+          }
+          my $sheetName = $sheet -> get_name();
+          my($row_min,$row_max)=$sheet -> row_range();
+          my($col_min,$col_max)=$sheet -> col_range();
+          for my $row ($row_min .. $row_max){
+              my @values;
+              for my $col ($col_min .. $col_max){
+                  my $cell = $sheet -> get_cell($row, $col);
+                  if(defined $cell and $cell -> value() ne ''){
+                      push @values, $cell -> value();
+                  }
+                  else{
+                      push @values, $$settings{sep};
+                  }
+              }
+              print join($$settings{sep},@values)."\n";
+          }
+      }
+      logmsg "XLS converted into TSV";
+  }
+
+  elsif($ext =~ /\.xlsx$/i){
+
+      my $workbook = Spreadsheet::XLSX -> new ($ARGV[0]);
+      foreach my $sheet (@{$workbook -> {Worksheet}}){
+          $sheetCounter++;
+          if($$settings{sheetnum} && $sheetCounter!=$$settings{sheetnum}){
+            next;
+          }
+          my $sheetName = $sheet->{Name};
+          $sheet -> {MaxRow} ||= $sheet -> {MinRow};    
+          foreach my $row ($sheet -> {MinRow} .. $sheet -> {MaxRow}){             
+              $sheet -> {MaxCol} ||= $sheet -> {MinCol};                
+              foreach my $col ($sheet -> {MinCol} .. $sheet -> {MaxCol}){
+                  my $cell = $sheet -> {Cells} [$row] [$col];                
+                  if($cell){
+                      print $cell -> {Val}, $$settings{sep};
+                  }
+                  else{
+                      print $$settings{sep}.$$settings{sep};
+                  }
+              }
+              print "\n";
+          }
+      }
+      logmsg "XLSX converted into TSV";
+  }
+
+  else{
+      logmsg "ERROR: unsupported extension in $file\n".
+             "This script requires a XLS or XLSX extension\n".
+             usage();
+  }
+
+  return 0;
 }
 
 sub usage{
-"Converts a Microsoft Excel spreadsheet into TSV format.
+"$0: Converts a Microsoft Excel spreadsheet into TSV format.
 Output is automatically created in same directory as the input.
 Each worksheet is written as a separate file.
 
-usage: $0 <inputfile>
+usage: $0 input.xls[x] > out.tsv
+--firstsheet        Print only the first sheet of a workbook.
+                    Synonym for --sheetnum=1
+--sheetnum    ''    Print only this sheet number (one-based)
+--separator   '\\t'  By default, tab-delimited output, but any
+                    string can be specified.
 ";
 }
