009b9200063811abb3a79b481cd13744213a0108
Lee Katz - Aspen <gzu2@cdc.gov>
2017-08-17 09:17:15 -0400

    checking for samtools version and also more executable deps

--- EXAMPLES/downloadAll.sh
+++ EXAMPLES/downloadAll.sh
@@ -14,22 +14,31 @@ function msg(){
 }
 
 msg "Looking for dependencies"
-for exe in esearch fastq-dump perl make GenFSGopher.pl run_snp_pipeline.sh; do
+for exe in esearch fastq-dump perl make GenFSGopher.pl run_snp_pipeline.sh \
+  bowtie2 tabix bgzip bcftools; do
   which $exe
 done
 # Need perl >= 5.12.0
 msg "Testing if perl >= 5.12.0 is in PATH"
 perl -e 'use 5.12.0; print "  -OK\n"'
+msg "Checking for samtools v1 or above"
+VERSION=$(samtools --version | grep samtools | grep -P -o '\d+(.\d+)?')
+VERSION=${VERSION:0:1}
+if (( $(echo "$VERSION < 1" | bc -l) )); then
+  msg "Found version $VERSION of samtools but need >= 1"
+  exit 1
+fi
+msg "  -OK"
 
 msg "Downloading datasets"
 for tsv in $THISDIR/../datasets/*.tsv; do
   name=$(basename $tsv .tsv)
   msg "Downloading $name"
-  GenFSGopher.pl --outdir $THISDIR/$name --layout cfsan --numcpus $NUMCPUS $tsv 
+  #GenFSGopher.pl --outdir $THISDIR/$name --layout cfsan --numcpus $NUMCPUS $tsv 
 
   msg "SNP-Pipeline"
   SNP_CONF="$THISDIR/$name/snppipeline.conf"
-  copy_snppipeline_data.py configurationFile $THISDIR/$name
+  #copy_snppipeline_data.py configurationFile $THISDIR/$name
 
   REF=$(ls $THISDIR/$name/reference/*.fasta | head -n 1)
   nice run_snp_pipeline.sh -c $SNP_CONF -s $THISDIR/$name/samples -m soft -o $THISDIR/$name/snp-pipeline $REF
